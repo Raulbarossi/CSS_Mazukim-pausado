b292ec92178479795c5f0e7dedcb1fc11003343f
Barossi <raul_barossi@hotmail.com>
2022-07-08 18:30:35 -0300

    Página de site offline

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <base href="/">
+  <link rel="shortcut icon" type="imagex/ico" href="view/img/favicon.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="view/css/indisponivel.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>Indisponível</title>
+</head>
+<body>
+  <main class="headerColor">
+      <div class="container pt-5 d-flex justify-content-center align-items-center header">
+        <div>
+          <a href="https://www.mazukim.com.br/">
+            <img src="view/img/mazukim2.png" class = "container-md">
+          </a>
+        </div>
+      </div>
+
+    <div class="corpo container-md d-flex justify-content-center align-items-center">
+      <img src="view/img/KIM_Modelo2D_PENSANDO(2).png" class="kim">
+      <div class="d-flex align-items-center janela">
+        <h2 class="texto"> O SITE <?= strtoupper($_SERVER['SERVER_NAME']) ?>  ESTÁ
+        PAUSADO POR MOTIVOS TÉCNICOS ENTRE EM CONTATO CONOSCO PARA ESCLARECER</h2>
+      </div>
+    </div>
+  </main>
+
+    <footer class="footer d-flex justify-content-center">
+      
+        <i class="align-self-end mb-3">
+          <a href="https://www.mazukim.com.br/">
+            <img src="view/img/mazukim.png" class = "logoRodape">
+          </a>
+        </i>
+      
+    </footer>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
